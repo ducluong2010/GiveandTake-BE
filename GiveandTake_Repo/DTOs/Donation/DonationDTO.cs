92f59e35d7802e5ab43ac0cd515044913b6c86a1
Nineh09 <100771455+Nineh09@users.noreply.github.com>
2024-10-09 20:05:11 +0700

    4 status

--- GiveandTake_Repo/DTOs/Donation/DonationDTO.cs
+++ GiveandTake_Repo/DTOs/Donation/DonationDTO.cs
@@ -56,4 +56,38 @@ namespace GiveandTake_Repo.DTOs.Donation
 
         public List<string>? DonationImages { get; set; }
     }
+    public class CreateDonationDTO
+    {
+        public int? AccountId { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public int? Point { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public int? ApprovedBy { get; set; }
+
+        public int? TotalRating { get; set; }
+
+        public List<string>? DonationImages { get; set; }
+    }
+    public class DonationFilterDTO
+    {
+        public int? CategoryId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int? Point { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public int? ApprovedBy { get; set; }
+        public string? Status { get; set; }
+    }
+
 }
--- Giveandtake_Services/Interfaces/IDonationServicecs.cs
+++ Giveandtake_Services/Interfaces/IDonationServicecs.cs
@@ -13,7 +13,12 @@ namespace Giveandtake_Services.Interfaces
         Task<IGiveandtakeResult> GetDonationById(int id);
         Task<IGiveandtakeResult> GetAllDonations(int page = 1, int pageSize = 8);
         Task<IGiveandtakeResult> UpdateDonation(int id, CreateUpdateDonationDTO donationInfo);
-        Task<IGiveandtakeResult> CreateDonation(CreateUpdateDonationDTO donationInfo);
+        Task<IGiveandtakeResult> CreateDonation(CreateDonationDTO donationInfo);
         Task<IGiveandtakeResult> DeleteDonation(int id);
+        Task<IGiveandtakeResult> ToggleDonationStatus(int id);
+        Task<IGiveandtakeResult> ToggleCancel(int id);
+        Task<IGiveandtakeResult> ToggleApproved(int id);
+        Task<IGiveandtakeResult> CheckAndUpdateAllBannedAccountsDonations();
+        Task<IGiveandtakeResult> CheckAndUpdateDonationsForActivatedAccounts();
     }
 }
