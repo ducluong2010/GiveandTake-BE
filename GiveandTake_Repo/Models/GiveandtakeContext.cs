ab8cf3d95f4c86533ffba62f310bbef2ecbc1a00
DucLuong <camkionline@gmail.com>
2024-10-21 11:31:35 +0700

    fix Context

--- GiveandTake_Repo/Models/GiveandtakeContext.cs
+++ GiveandTake_Repo/Models/GiveandtakeContext.cs
@@ -46,7 +46,7 @@ public partial class GiveandtakeContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=103.163.25.35;port=3306;database=giveandtake;uid=admin;pwd=Luong123_A", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.39-mysql"));
+        => optionsBuilder.UseMySql("server=103.163.25.35;port=3306;database=giveandtake;uid=admin;pwd=Luong123_A", Microsoft.EntityFrameworkCore.ServerVersion.Parse("9.0.1-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
