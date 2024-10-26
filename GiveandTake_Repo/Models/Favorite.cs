c9cb4039fd0889ad4391b08df548def0d63b4e58
DucLuong <camkionline@gmail.com>
2024-10-26 10:02:49 +0700

    New Scaffold

---
+++ GiveandTake_Repo/Models/Favorite.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GiveandTake_Repo.Models;
+
+public partial class Favorite
+{
+    public int FavoriteId { get; set; }
+
+    public int? AccountId { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public virtual Account? Account { get; set; }
+
+    public virtual Category? Category { get; set; }
+}
