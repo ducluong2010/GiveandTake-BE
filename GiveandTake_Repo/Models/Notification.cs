e48dc3f05030f70459457ec843ebec3f840723f3
DucLuong <camkionline@gmail.com>
2024-10-28 00:09:11 +0700

    Notification

---
+++ GiveandTake_Repo/Models/Notification.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GiveandTake_Repo.Models;
+
+public partial class Notification
+{
+    public int NotificationId { get; set; }
+
+    public int? DonationId { get; set; }
+
+    public int? AccountId { get; set; }
+
+    public string? Note { get; set; }
+
+    public string? Type { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public bool? IsRead { get; set; }
+
+    public int? StaffId { get; set; }
+}
